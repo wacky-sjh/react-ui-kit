cf7cc96c7e56c2c7826164841076498703f3411b
wacky-sjh <sjh@wacky.kr>
2025-06-11 18:35:16 +0900

    first commit

---
+++ src/components/common/Button.stories.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+};
+
+export const Primary = () => <Button>Primary</Button>;
---
+++ src/components/common/Button.tsx
@@ -0,0 +1,5 @@
+import { Button as ButtonPrimitive } from "@/components/ui/button";
+
+export const Button = () => {
+  return <ButtonPrimitive className="bg-blue-500 text-white">Button</ButtonPrimitive>;
+};
