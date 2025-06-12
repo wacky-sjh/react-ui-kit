11ecd92a3c17a9ca9dd93ecd38192e8776899edf
wacky-sjh <sjh@wacky.kr>
2025-06-12 18:10:38 +0900

    Feat: 샘플 제품 이미지 및 인덱스 파일 추가

---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+export { default as SampleProduct0 } from "./images/samples/sample-product0.jpg";
+export { default as SampleProduct1 } from "./images/samples/sample-product1.jpg";
+export { default as SampleProduct2 } from "./images/samples/sample-product2.jpg";
+export { default as SampleProduct3 } from "./images/samples/sample-product3.jpg";
